3b968092ff7d6c45189613653788e2aa969329d4
aguiagui5 <68275619+aguiagui5@users.noreply.github.com>
2020-07-15 13:29:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -8,48 +8,30 @@ if('serviceWorker' in navigator){
     console.log('No puedes usar sw en tu navegador');
 }
 
-año=2020;
-mes=7;
-dia=14;
-hora=20;
-minuto=39;
-segundo=0;
-urlsitio = 'file:///C:/wamp64/www/curso-pwa/index.html'; //url del sitio web final			
-
-dateFuture = new Date(año, mes-1 , dia, hora, minuto, segundo);		
-window.onload=function(){GetCount();}
-
-function GetCount(){
-		
-    dateNow = new Date();                                             
-    amount = dateFuture.getTime() - dateNow.getTime();                
-    delete dateNow;
-
-    if(amount < 0){
-            location.href=urlsitio;
-    }
-    else{
-            days=0;hours=0;mins=0;secs=0;out="";
-
-            amount = Math.floor(amount/1000);
-
-            days=Math.floor(amount/86400);
-            amount=amount%86400;
-
-            hours=Math.floor(amount/3600);
-            amount=amount%3600;
-
-            mins=Math.floor(amount/60);
-            amount=amount%60;
-
-            secs=Math.floor(amount);
-
-            if(days != 0){out += days +" dia"+((days!=1)?"s":"")+", ";}
-            if(days != 0 || hours != 0){out += hours +" hora"+((hours!=1)?"s":"")+", ";}
-            if(days != 0 || hours != 0 || mins != 0){out += mins +" minuto"+((mins!=1)?"s":"")+", ";}
-            out += secs +" segundos";
-            document.getElementById('countbox').innerHTML=out;
-
-            setTimeout("GetCount()", 1000);
-    }
-}
+var text2= 'hola'
+            function validate(text1)
+            
+            {
+            
+             if (text1==text2)
+            
+             load('videos/video2.mp4');
+            
+             else
+            
+             {
+            
+                alert("Antídoto incorrecto");
+            
+             }
+            
+            }
+            
+            function load(url)
+            
+            {
+            
+             location.href=url;
+            
+            }
+            
